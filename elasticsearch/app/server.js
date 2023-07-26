cb35053f50ae8b34b285e3502644fb7bf15bb1f9
Marcus Chavez <mchavez@newrelic.com>
2023-07-25 17:02:05 -0700

    style: add prettier styling

--- elasticsearch/app/server.js
+++ elasticsearch/app/server.js
@@ -1,45 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 const elasticClient = require("./elastic-client");
 
 const createIndex = async (indexName) => {
   await elasticClient.indices.create({
-    index: indexName
+    index: indexName,
   });
-}
+};
 
 const indexExists = async (indexName) => {
   const result = await elasticClient.indices.exists({
-    index: indexName
+    index: indexName,
   });
 
   return result;
-}
+};
 
 const postExists = async (indexName, postId) => {
   const result = await elasticClient.exists({
     id: postId,
-    index: indexName
-  })
+    index: indexName,
+  });
 
   return result;
-}
+};
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get('/es-demo', (req, res) => {
-  res.status(200).json({ message: 'Elasticsearch Skeleton reporting for duty! 💀'});
-})
+app.get("/es-demo", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Elasticsearch Skeleton reporting for duty! 💀" });
+});
 
 app.post("/create-post", async (req, res) => {
-  const duplicatePost = await postExists("posts", "My document")
+  const duplicatePost = await postExists("posts", "My document");
 
   if (duplicatePost) {
-    res.status(409).json({message: "Document already exists"})
+    res.status(409).json({ message: "Document already exists" });
     return;
   }
 
@@ -47,16 +49,16 @@ app.post("/create-post", async (req, res) => {
     index: "posts",
     id: "My document",
     document: {
-      title: "My document"
-    }
+      title: "My document",
+    },
   });
 
-  res.status(201).json({message: "Document created"})
-})
+  res.status(201).json({ message: "Document created" });
+});
 
 app.listen(port, () => {
   if (!indexExists("posts")) {
     createIndex("posts");
   }
   console.log(`Example app listening on port ${port}`);
-})
+});
