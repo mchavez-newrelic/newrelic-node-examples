7c945f50563b922dd38abe8b4495087ece689c20
Marcus Chavez <mchavez@newrelic.com>
2023-08-01 12:36:41 -0700

    chore: use localhost node instead of Docker service name

--- elasticsearch/app/elastic-client.js
+++ elasticsearch/app/elastic-client.js
@@ -2,7 +2,7 @@ const { Client } = require("@elastic/elasticsearch");
 require("dotenv");
 
 const elasticClient = new Client({
-  node: "http://elasticsearch:9200",
+  node: "http://localhost:9200",
   auth: {
     username: process.env.ELASTIC_USERNAME,
     password: process.env.ELASTIC_PASSWORD,
