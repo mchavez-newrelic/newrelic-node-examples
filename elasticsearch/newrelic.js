4e7a8c9ea279cc8d9c28eb598818244c0609a8cf
Marcus Chavez <mchavez@newrelic.com>
2023-08-01 12:35:47 -0700

    chore: add trace level logging

--- elasticsearch/newrelic.js
+++ elasticsearch/newrelic.js
@@ -1,6 +1,14 @@
 require("dotenv");
 
 exports.config = {
-    app_name: 'Demo Elasticsearch Application',
-    license_key: process.env.LICENSE_KEY
-}
+  app_name: "Demo Elasticsearch Application",
+  license_key: process.env.LICENSE_KEY,
+  api: {
+    esm: {
+      custom_instrumentation_entrypoint: "/app/instrumentation.js",
+    },
+  },
+  logging: {
+    level: "trace"
+  }
+};
