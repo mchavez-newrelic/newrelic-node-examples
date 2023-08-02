615c23a1e6414792ddd50573de41318c22e9c841
Marcus Chavez <mchavez@newrelic.com>
2023-08-02 15:31:42 -0700

    chore: record attributes for connection parameters

--- elasticsearch/app/instrumentation.js
+++ elasticsearch/app/instrumentation.js
@@ -5,12 +5,10 @@ newrelic.instrumentDatastore('@elastic/elasticsearch', instrumentElastic);
 
 function instrumentElastic(shim, elastic, moduleName) {
     function queryParser(params) {
-        debugger
         // TODO: depending on the elastic search request the params need 
         // further logic applied to determine the operation
         params = JSON.parse(params);
         const path = params.path.split('/');
-        debugger
         return {
             collection: path?.[1],
             operation: path?.[2]?.replace('_',''),
@@ -23,25 +21,29 @@ function instrumentElastic(shim, elastic, moduleName) {
     
     // cwd is node_modules/@elastic/elasticsearch w/ shim.require so use the relative path
     const Transport = elastic.Transport;
-    // const ClientLibrary = elastic.Client;
     
-    // shim.recordOperation(ClientLibrary.BaseConnectionPool.prototype, 'addConnection', function wrapAddConnection(shim, fn, fnName, args) {
-    //     debugger
+    // shim.recordOperation(Transport.prototype, 'addConnection', function (shim, addConnection, name, args) {
     //     const parameters = {'host': args[0]};
-    //     const newArgs = [args[0], fn];
     
     //     return {
-    //         fnName,
+    //         name,
     //         parameters,
     //         promise: true
     //     }
     // })
 
     shim.recordQuery(Transport.prototype, 'request', function(shim, request, name, args) {
-        debugger
+        // simply grab the first connection in the connection pool
+        const connection = this.connectionPool.connections[0];
         return {
             query: JSON.stringify(args?.[0]),
-            promise: true
+            promise: true,
+            inContext: function inContext() {
+                shim.captureInstanceAttributes(
+                    connection.url.toString(),
+                    9200,
+                )
+            },
         }
     });
 }
