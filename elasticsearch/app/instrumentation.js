fc4725d6a281739cfd68e29330e52933debc1bba
Marcus Chavez <mchavez@newrelic.com>
2023-08-02 11:12:09 -0700

    chore: basic query instrumentation working

--- elasticsearch/app/instrumentation.js
+++ elasticsearch/app/instrumentation.js
@@ -4,31 +4,44 @@ const newrelic = require('newrelic');
 newrelic.instrumentDatastore('@elastic/elasticsearch', instrumentElastic);
 
 function instrumentElastic(shim, elastic, moduleName) {
-    shim.setDatastore("elastic");
-    // cwd is node_modules/@elastic/elasticsearch w/ shim.require so use the relative path
-    const TransportLibrary = shim.require('../../@elastic/transport');
-    const ClientLibrary = shim.require('../../@elastic/elasticsearch');
-    
-    shim.recordOperation(ClientLibrary.BaseConnectionPool.prototype, 'addConnection', function wrapAddConnection(shim, fn, fnName, args) {
+    function queryParser(params) {
         debugger
-        const parameters = {'host': args[0]};
-        const newArgs = [args[0], fn];
-    
+        // TODO: depending on the elastic search request the params need 
+        // further logic applied to determine the operation
+        params = JSON.parse(params);
+        const path = params.path.split('/');
+        debugger
         return {
-            fnName,
-            parameters,
-            promise: true
+            collection: path?.[1],
+            operation: path?.[2]?.replace('_',''),
+            query: JSON.stringify(params?.body?.query)?.replaceAll('"',''),
         }
-    })
+    }
 
-    shim.recordQuery(elastic.Transport.prototype, 'request', function wrapRequest() {
+    shim.setDatastore("elastic");
+    shim.setParser(queryParser);
+    
+    // cwd is node_modules/@elastic/elasticsearch w/ shim.require so use the relative path
+    const Transport = elastic.Transport;
+    // const ClientLibrary = elastic.Client;
+    
+    // shim.recordOperation(ClientLibrary.BaseConnectionPool.prototype, 'addConnection', function wrapAddConnection(shim, fn, fnName, args) {
+    //     debugger
+    //     const parameters = {'host': args[0]};
+    //     const newArgs = [args[0], fn];
+    
+    //     return {
+    //         fnName,
+    //         parameters,
+    //         promise: true
+    //     }
+    // })
+
+    shim.recordQuery(Transport.prototype, 'request', function(shim, request, name, args) {
         debugger
-        return function wrappedRequest() {
-            const args = shim.argsToArray.apply(shim, arguments);
-            return {
-                query: args[3][0].body.query.fuzzy.title,
-                promise: true
-            }
+        return {
+            query: JSON.stringify(args?.[0]),
+            promise: true
         }
     });
 }
